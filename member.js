faf0b44998decc2468cded39986e7ce7efd3eb4d
angcm25 <99634830+MerariPerdomo@users.noreply.github.com>
2023-11-28 04:27:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember(){
+    return{
+        restrict: 'E',
+        templateUrl: 'directives/member.html',
+        controller: 'SkillsMemberCtrl',
+        controllerAs: 'memberCtrl',
+        bindToController: true,
+        scope: {
+          member: '='
+        }
+      };
+}
