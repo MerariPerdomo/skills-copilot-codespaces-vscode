fb51888bacede9a3180daddb37ab900975a85e4e
angcm25 <99634830+MerariPerdomo@users.noreply.github.com>
2023-11-28 05:03:47 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,51 +1,38 @@
-//create a web server with node.js
-//-----------------------------
-const http = require('http');
+// create web server 
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const comments = require('./comments.json');
 const fs = require('fs');
-const url = require('url');
-const qs = require('querystring');
-//-----------------------------
-// Web server
-//-----------------------------
-http.createServer(function (req, res) {
-    //console.log(req.url);
-    //console.log(req.method);
-    //-----------------------------
-    // Routing
-    //-----------------------------
-    const parsedUrl = url.parse(req.url);
-    //console.log(parsedUrl);
-    //-----------------------------
-    // Routing
-    //-----------------------------
-    switch (parsedUrl.pathname) {
-        case '/':
-            fs.readFile('index.html', 'utf-8', function (err, data) {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(data);
-                res.end();
-            }); //end of readFile
-            break;
-        case '/about':
-            fs.readFile('about.html', 'utf-8', function (err, data) {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(data);
-                res.end();
-            }); //end of readFile
-            break;  
-        case '/contact':
-            fs.readFile('contact.html', 'utf-8', function (err, data) {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(data);
-                res.end();
-            }); //end of readFile
-            break;
-        case '/login':
-            fs.readFile('login.html', 'utf-8', function (err, data) {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(data);
-                res.end();
-            }); //end of readFile
-            break;
-    }
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+app.post('/comments', (req, res) => {
+    // get the data from the client
+    const newComment = req.body;
+    // add the data to the comments.json file
+    comments.push(newComment);
+    // write the data back to the file
+    fs.writeFile('comments.json', JSON.stringify(comments), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+    // send a response to the client
+    res.json({ msg: 'Thanks for your message' });
+});
+
+// Start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
